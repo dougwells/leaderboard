9cec50befff7d3b8416c7940aac59ff01df32613
dougwells <dougwells@gmail.com>
2015-11-09 20:40:18 -0700

    Added button to increment score

--- leaderboard.js
+++ leaderboard.js
@@ -19,6 +19,14 @@ if (Meteor.isClient){
     Template.leaderboard.events({
         'click li.player': function(){
             Session.set('selectedPlayer', this._id);
+        },
+
+        'click #increment': function(){
+            var id = Session.get('selectedPlayer');
+            var newScore = 5+ PlayersList.findOne({_id:id}).score;
+            PlayersList.update({_id:id}, {$set:{score: newScore}});
+            console.log(newScore);
+
         }
     });
 
